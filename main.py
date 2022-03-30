daec488c94a11cd00499f1520a1bb888ec0f04a9
StackSpot Admin <admin@stackspot.com>
2022-03-30 03:03:28 +0000

    Update public repository

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import subprocess
 import json
 import os
 
-def put_appsettings(metadata: Metadata, project_name: str, exporter_type: str, file_name: str):
-        os.chdir(f'{metadata.target_path}/src/{project_name}.Api/')
+def put_appsettings(metadata: Metadata, target_path: str, exporter_type: str, file_name: str):
+        os.chdir(target_path)
         print(f'Setting {file_name}...')
 
         if exporter_type == 'X-Ray - Daemon':
@@ -43,37 +43,48 @@ class Plugin(Template):
     def post_hook(self, metadata: Metadata):
         project_name = metadata.global_inputs['project_name']
         exporter_type = metadata.inputs['exporter_type']
-
-        os.chdir(f'{metadata.target_path}/src/{project_name}.Domain/')
-        
+       
         if exporter_type == 'X-Ray - Daemon':
+            os.chdir(f'{metadata.target_path}/src/{project_name}.Application/')
             subprocess.run(['dotnet', 'add', 'package', 'StackSpot.Tracing.XRay'])
-            using = f"using StackSpot.Tracing.XRay;\n"
+
+            os.chdir(f'{metadata.target_path}/src/{project_name}.Infrastructure/')
+            subprocess.run(['dotnet', 'add', 'package', 'StackSpot.Tracing.XRay'])            
+
+            using = f"using StackSpot.Tracing.XRay;"
             service = f"services.AddXRay(configuration);"
             app = f"app.UseXRay(configuration[\"AppName\"]);"
         else:
+            os.chdir(f'{metadata.target_path}/src/{project_name}.Application/')
             subprocess.run(['dotnet', 'add', 'package', 'StackSpot.Tracing'])
-            using = f"using StackSpot.Tracing;\n"
+
+            os.chdir(f'{metadata.target_path}/src/{project_name}.Infrastructure/')
+            subprocess.run(['dotnet', 'add', 'package', 'StackSpot.Tracing'])
+
+            using = f"using StackSpot.Tracing;"
             service = f"services.AddOpenTelemetryTracing(configuration);"
 
         os.chdir(f'{metadata.target_path}/src/{project_name}.Api/')
 
-        put_appsettings(metadata, project_name, exporter_type, 'appsettings.json')
-        put_appsettings(metadata, project_name, exporter_type, 'appsettings.Development.json')         
-               
+        put_appsettings(metadata, f'{metadata.target_path}/src/{project_name}.Api/', exporter_type, 'appsettings.json')
+        put_appsettings(metadata, f'{metadata.target_path}/src/{project_name}.Api/', exporter_type, 'appsettings.Development.json')         
+        put_appsettings(metadata, f'{metadata.target_path}/tests/{project_name}.Api.IntegrationTests/', exporter_type, 'appsettings.json')
+
         print('Setting Configuration...')
 
-        configuration_file = open(file='ConfigurationStackSpot.cs', mode='r')
+        os.chdir(f'{metadata.target_path}/src/{project_name}.Application/Common/StackSpot/')
+        configuration_file = open(file='DependencyInjection.cs', mode='r')
         content = configuration_file.readlines()
+        index_using = [x for x in range(len(content)) if 'using' in content[x].lower()]
         index = [x for x in range(len(content)) if 'return services' in content[x].lower()]
-        content[0] = using+content[0]
+        content[index_using[0]] = f"{using}\n{content[index_using[0]]}"
         content[index[0]] = f"{service}\n{content[index[0]]}"
 
         if exporter_type == 'X-Ray - Daemon':
             index_app = [x for x in range(len(content)) if 'return app' in content[x].lower()]            
             content[index_app[0]] = f"{app}\n{content[index_app[0]]}"     
         
-        configuration_file = open(file='ConfigurationStackSpot.cs', mode='w')                     
+        configuration_file = open(file='DependencyInjection.cs', mode='w')                     
         configuration_file.writelines(content)
         configuration_file.close()
 
@@ -81,7 +92,7 @@ class Plugin(Template):
 
         print('Apply dotnet format...')
         os.chdir(f'{metadata.target_path}/')
-        subprocess.run(['dotnet', 'dotnet-format', './src'])   
+        subprocess.run(['dotnet', 'dotnet-format', f'src/{project_name}.Application/{project_name}.Application.csproj', '--include-generated'])   
         print('Apply dotnet format done...')
 
 if __name__ == '__main__':
